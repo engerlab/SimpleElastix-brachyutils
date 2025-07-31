658726a2d5e4fa6fcfb3f464a10595807d59ca7e
hosseinjafar <hosseinj.2555@gmail.com>
2025-07-31 13:57:25 -0400

    finally done with all the setup needed for docker

--- docker_src/command_docker.sh
+++ docker_src/command_docker.sh
@@ -10,10 +10,10 @@ export XAUTHORITY=${HOME}/.Xauthority
 export DOCKER_BUILDKIT=1
 echo $(ssh-agent)
 ssh-add ~/.ssh/id_rsa # to allow the container to access the host SSH keys
-docker compose up --build -d SimpleElastix
+# docker compose up --build -d SimpleElastix
 
 # # to run the container without building the image
-# docker compose up --no-build -d SimpleElastix
+docker compose up --no-build -d SimpleElastix
 
 # # to enter the container
 docker exec -it SimpleElastix bash
